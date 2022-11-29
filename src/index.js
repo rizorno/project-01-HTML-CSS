227811b2650a58228f4ae76cd50229d3a0f0755d
rizorno <114782234+rizorno@users.noreply.github.com>
2022-11-29 03:54:15 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Swiper, { Pagination } from 'swiper';
+
+Swiper.use([Pagination]);
+
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-custom-pagination',
+    clickable: true,
+  },
+});
