77ef6489a6979ecf97765327e752007c5447a1fe
rizorno <114782234+rizorno@users.noreply.github.com>
2022-11-30 07:54:38 +0000

    Deploying to gh-pages from @ rizorno/project-01-HTML-CSS@a62b044da0596c7ac50ad480dfb6629e2d7e6b30 🚀

---
+++ index.22612e29.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-custom-pagination",clickable:!0}});
+//# sourceMappingURL=index.22612e29.js.map
---
+++ index.960fb908.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-custom-pagination",clickable:!0}});
+//# sourceMappingURL=index.960fb908.js.map
