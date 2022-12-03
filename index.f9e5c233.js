dbea937ae9626bc01e2d2c0c36ade93ec36ce40a
rizorno <114782234+rizorno@users.noreply.github.com>
2022-12-03 13:22:32 +0000

    Deploying to gh-pages from @ rizorno/project-01-HTML-CSS@7034fad51183e25b55d165ca1da523b100b96e5e 🚀

---
+++ index.f9e5c233.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-video-open]"),closeModalBtn:document.querySelector("[data-video-close]"),modal:document.querySelector("[data-video]")};function o(){e.modal.classList.toggle("is-hidden-video")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.f9e5c233.js.map
